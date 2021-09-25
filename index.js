1455c451b9fa3f71c16aa9092bd7bbd8b0ef63c4
gilbertomoj <gibamedeirosgc@gmail.com>
2021-09-25 16:03:44 -0300

    Atividades

---
+++ index.js
@@ -0,0 +1,34 @@
+// var fruta = "Laranja"
+// switch (fruta){
+//     case "maça":
+//         console.log("Não vendemos esta fruta aqui")
+//         break
+//     case "kiwi":
+//         console.log("Estamos com escassez de kiwis")
+//         break
+//     case "melancia":
+//         console.log("Aqui está, são 3 reais o quilo")
+//         break
+//     default:
+//         console.log("Procure o administrador do sistema!")
+//         break
+// }
+
+// Atividade 2 
+// var salario = 280.00
+// var mult = 0
+// if(salario <= 280.00){
+//     mult = 0.20
+//     console.log("O salário antes do reajuste : "+salario+"\nO percentual de aumento aplicado : "+(mult * 100)+"%\nO valor do aumento : "+(salario*mult)+"\nO novo salário, após o aumento : "+(salario*mult)+salario)
+// }else if(salario > 280 && salario <= 700){
+//     mult = 0.15
+//     console.log("O salário antes do reajuste : "+salario+"\nO percentual de aumento aplicado : "+(mult * 100)+"%\nO valor do aumento : "+(salario*mult)+"\nO novo salário, após o aumento : "+(salario*mult)+salario)
+// }else if(salario > 700 && salario <= 1500){
+//     mult = 0.10
+//     console.log("O salário antes do reajuste : "+salario+"\nO percentual de aumento aplicado : "+(mult * 100)+"%\nO valor do aumento : "+(salario*mult)+"\nO novo salário, após o aumento : "+(salario*mult)+salario)
+// }else if(salario > 1500){
+//     mult = 0.05
+//     console.log("O salário antes do reajuste : "+salario+"\nO percentual de aumento aplicado : "+(mult * 100)+"%\nO valor do aumento : "+(salario*mult)+"\nO novo salário, após o aumento : "+(salario*mult)+salario)
+// }
+
+//Atividade 3
